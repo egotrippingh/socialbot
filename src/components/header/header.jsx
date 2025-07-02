01c0de171965ef8e13e0e622f530e64440816ed7
egotrippingh <egorjojori@gmail.com>
2025-07-02 15:04:23 +0300

    fonts etc

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,27 +2,27 @@ import './header.scss';
 
 export const HeaderNavigation = () => {
   const navbar = [
-    { id: 1, label: 'имя1', path: '/' },
-    { id: 2, label: 'имя2', path: '/' },
-    { id: 3, label: 'имя3', path: '/' },
-    { id: 4, label: 'имя4', path: '/' },
+    { id: 1, label: 'О нас', path: '/' },
+    { id: 2, label: 'Функционал', path: '/' },
+    { id: 3, label: 'Мануал по использованию', path: '/' },
+    { id: 4, label: 'Прайсинг', path: '/' },
   ];
 
   return (
     <>
     <header className='header'>
+      <button className='header__logo'><a href="/image"><img src="#" alt="#" /></a></button>
       <nav className='header__nav'>
-        <button><a href="/image">logo</a></button>
         {navbar.map((item) => (
           <button className="button" key={item.id}>
             <a href={item.path}>{item.label}</a>
           </button>
         ))}
-      </nav>
-      <div className='header__autorize'>
+        <div className='header__autorize'>
         <button className='button'>Вход</button>
         <button className='button'>Регистрация</button>
         </div>
+      </nav>
         </header>
     </>
   );
