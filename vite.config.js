facf680ab7ae1d7105afc434d07e1b8eadd1a294
egotrippingh <tripplesix@inbox.ru>
2025-07-03 23:34:43 +0300

    Please enter the commit message for your changes.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [
+      {
+        find: "@/",
+        replacement: path.resolve("src") + "/",
+      },
+    ],
   },
   css: {
     preprocessorOptions: {
       scss: {
-        // Автоматически импортируем переменные и миксины
-        additionalData: `
-          @use "@/styles/helpers" as *;
-        `,
+        additionalData: `@use "@/styles/helpers" as *;`,
       },
     },
   },
