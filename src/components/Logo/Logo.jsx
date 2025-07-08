08119b5e5760ec2f354ae5868a9ff56dd6a93119
egotrippingh <tripplesix@inbox.ru>
2025-07-08 23:14:04 +0300

    header component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,29 @@
+import './Logo.scss'
+import classNames from 'classnames'
+
+const Logo = (props) => {
+  const {
+    className,
+    loading = 'lazy',
+  } = props
+  const title = 'Home'
+  return (
+    <a
+      className={classNames(className, 'logo')}
+      href="/"
+      title={title}
+      aria-label={title}
+    >
+      <img
+        className="logo__image"
+        src="/public/logo.svg"
+        alt="logo"
+        width={153}
+        height={24}
+        loading={loading}
+      />
+    </a>
+  )
+}
+
+export default Logo
--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,35 +1,30 @@
 import './Header.scss'
 import {Link} from "react-router-dom";
+import Logo from "@/components/Logo/index.js";
 
 const Header = () => {
 
   const navItems = [
-    {id:1, label:'Главная', path: '/'},
-    {id:2, label:'О сервисе', path: '/about'},
-    {id:3, label:'Гайд', path: '/guide'},
-    {id:4, label:'Поддержка', path: '/support'},
-    {id:5, label:'Прайсинг', path: '/pricing'}
+    {id: 1, label: 'Главная', path: '/'},
+    {id: 2, label: 'О сервисе', path: '/about'},
+    {id: 3, label: 'Гайд', path: '/guide'},
+    {id: 4, label: 'Поддержка', path: '/support'},
+    {id: 5, label: 'Прайсинг', path: '/pricing'}
   ];
 
   return (
     <header className='header'>
-      <div className="header__inner container">
-        <img className='header__logo'
-             src="@/assets/images/socialbot.webp"
-             alt="logo"
-        />
-        {navItems.map(item => (
-          <nav>
-            <li key={item.id}>
-              <a href={item.path}>
-                {item.label}
-              </a>
-            </li>
-          </nav>
-        ))}
-      </div>
+      <Logo loading = {'eager'} />
+      {navItems.map(item => (
+        <nav>
+          <li key={item.id}>
+            <a href={item.path}>
+              {item.label}
+            </a>
+          </li>
+        </nav>
+      ))}
     </header>
   )
 }
-
 export default Header
