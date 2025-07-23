2a93f53dec596882052cddd11358f9dd6eb93b7c
egotrippingh <tripplesix@inbox.ru>
2025-07-23 10:15:42 +0300

    sidebar halfprocess, browser router, react useLocation

--- src/global.jsx
+++ src/global.jsx
@@ -1,12 +1,18 @@
 import {createRoot} from 'react-dom/client'
+import {BrowserRouter} from "react-router-dom";
 import '@/styles'
 import Sidebar from "@/layouts/Sidebar/Sidebar.jsx";
 import Content from "@/layouts/Content/Content.jsx";
 import Footer from "@/layouts/Footer/Footer.jsx";
 
 
-createRoot(document.getElementById('root')).render(<>
-  <Sidebar />
-  <div className="container">
-  </div>
-</>)
+createRoot(document.getElementById('root')).render(
+  <>
+  <BrowserRouter>
+    <Sidebar />
+    <div className="container">
+
+    </div>
+  </BrowserRouter>
+</>
+)
--- src/layouts/Sidebar/Sidebar.jsx
+++ src/layouts/Sidebar/Sidebar.jsx
@@ -1,7 +1,10 @@
 import './Sidebar.scss'
 import Logo from "@/components/Logo/index.js";
+import {Link, useLocation} from 'react-router-dom'
 
 const Sidebar = () => {
+
+  const {pathname} = useLocation()
 
   const navItems = [
     {id: 1, label: 'Главная', path: '/'},
@@ -11,28 +14,32 @@ const Sidebar = () => {
     {id: 5, label: 'Прайсинг', path: '/pricing'}
   ];
 
+
   return (
     <aside className='sidebar'>
       <div className='sidebar__block'>
+        <button className='sidebar__icon'>
+
+        </button>
         <Logo
           loading={'eager'}
-          className={"sidebar__logo"}
+          className={`sidebar__logo ${pathname ==='/'?'is-active':''}`}
         />
       </div>
       <div className='sidebar__block'>
         <div className="sidebar__block-middle">
           <nav>
             <ul>
-              {navItems.map(item => (
-                <li
-                  className='sidebar__navigation-links'
-                  key={item.id}
-                >
-                  <a className='sidebar__navigation-link' href={item.path}>
-                    {item.label}
-                  </a>
-                </li>
-              ))}
+              {navItems.map(item => {
+                const isActiveLink = pathname === item.path;
+                return (
+                  <li className='sidebar__navigation-links' key={item.id}>
+                    <Link to={item.path} className={`sidebar__navigation-link ${isActiveLink ? 'link-is-active' :''}`}>
+                      {item.label}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </nav>
         </div>
