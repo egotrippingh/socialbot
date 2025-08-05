1e0d42d652cdac959d057cb599ec1ac99375f8d2
egotrippingh <tripplesix@inbox.ru>
2025-08-05 22:59:50 +0300

    sidebar ended but need zustand

--- src/layouts/Sidebar/Sidebar.jsx
+++ src/layouts/Sidebar/Sidebar.jsx
@@ -1,65 +1,46 @@
-import './Sidebar.scss'
-import Logo from "@/components/Logo/index.js";
-import {Link, useLocation} from 'react-router-dom'
-import {useState} from "react";
+import './Sidebar.scss';
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const Sidebar = () => {
-
-  const {pathname} = useLocation()
+  const { pathname } = useLocation();
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   const navItems = [
-    {id: 1, label: 'Главная', path: '/'},
-    {id: 2, label: 'О сервисе', path: '/about'},
-    {id: 3, label: 'Гайд', path: '/guide'},
-    {id: 4, label: 'Поддержка', path: '/support'},
-    {id: 5, label: 'Прайсинг', path: '/pricing'}
+    { id: 1, icon: '/homepage.svg', label: 'Главная', path: '/' },
+    { id: 2, icon: '/info.svg', label: 'О сервисе', path: '/about' },
+    { id: 3, icon: '/user_guide.svg', label: 'Гайд', path: '/guide' },
+    { id: 4, icon: '/support_agent.svg', label: 'Поддержка', path: '/support' },
+    { id: 5, icon: '/payments.svg', label: 'Прайсинг', path: '/pricing' },
   ];
 
-  const [isActiveSidebar, setActiveSidebar] = useState(true)
-
   return (
-    <aside className='sidebar'>
-      <div className='sidebar__block'>
-        <Logo
-          loading={'eager'}
-          className={`sidebar__logo ${pathname ==='/'?'logo-active':''}`}
-        />
-        <button
-          onClick={() => setActiveSidebar(prev => !prev)}
-          className={`sidebar__icon`}
-        >
-          <img loading="eager" alt='toggleSidebar'
-               src={isActiveSidebar
-                 ? '/chevron_left.svg'
-                 : '/chevron_right.svg'} />
-        </button>
+    <aside className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+      <div className='sidebar__logo'>
+        <span className='sidebar__logo-inner'>s</span>
+        <span className={`sidebar__logo-inner ${isCollapsed ? 'sidebar__logo-collapsed' : ''}`}>ocial</span>
+        <span className='sidebar__logo-inner sidebar__logo-innerR'>R</span>
       </div>
-      <div className='sidebar__block'>
-        <div className="sidebar__block-middle">
-          <nav>
-            <ul>
-              {navItems.map(item => {
-                const isActiveLink = pathname === item.path;
-                return (
-                  <li className='sidebar__navigation-links' key={item.id}>
-                    <Link to={item.path} className={`sidebar__navigation-link ${isActiveLink ? 'link-is-active' :''}`}>
-                      {item.label}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </div>
-      </div>
-      <div className='sidebar__block'>
-        <a>
-          Вход
-        </a>
-      </div>
+
+      <button onClick={() => setIsCollapsed(!isCollapsed)} className='sidebar__setActive'>
+        <img src='/chevron_left.svg' alt='toggle' />
+      </button>
+
+      <nav className='sidebar__navigation'>
+        <ul className='sidebar__navigation-block'>
+          {navItems.map(({ id, icon, label, path }) => (
+            <li key={id} className='sidebar__navigation-element'>
+              <div className={`sidebar__navigation-markup ${pathname === path ? 'active__markup' : ''}`}></div>
+              <Link to={path} className='sidebar__navigation-link'>
+                <img src={icon} alt={label} />
+                <span>{label}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </aside>
-  )
-}
+  );
+};
 
-
-export default Sidebar
+export default Sidebar;
